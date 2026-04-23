e97963021acc6549df3a163386871d8673a2a7f8
Peter (Rocky) Villano <github@saej.in>
2026-04-22 23:04:52 -0400

    guarantee an update, even if no work was performed

--- src/followsFollows.ts
+++ src/followsFollows.ts
@@ -13,6 +13,8 @@ export function stopFollowsFollowsFollows(): void {
   lock = performance.now() // everything started in the past stops
 }
 
+const globalFollowsMap = new Map<string, string[]>()
+
 export async function followsFollows(
   actor: string,
   setters: {
@@ -35,17 +37,65 @@ export async function followsFollows(
   lock = Math.max(lock, startTime) // most recent wins
   let lastUpdate = -2000;
 
-  const myFollowsResponse = await getAllFollows(actor)
-  if (!myFollowsResponse.success) {
-    throw new Error("Failed to fetch your follows")
+  let myFollowsList = globalFollowsMap.get(actor)
+
+  if(myFollowsList === undefined) {
+    const myFollowsResponse = await getAllFollows(actor)
+    if (!myFollowsResponse.success) {
+      throw new Error("Failed to fetch your follows")
+    }
+    myFollowsList = myFollowsResponse.data.follows
+    globalFollowsMap.set(actor, myFollowsList)
   }
-  const myFollowsList = myFollowsResponse.data.follows
+
   followsMap.set(actor, myFollowsList)
   setMyFollowIds(new Set(myFollowsList))
 
-
   const workQueue: { actor: string, work: ReturnType<typeof getFollows> }[] = myFollowsList
+    .filter(e => {
+      const existingEntry = globalFollowsMap.get(e)
+      if(existingEntry) {
+        followsMap.set(e, existingEntry)
+        return false
+      }
+      return true
+    })
     .map(e => ({actor: e, work: getFollows(e)}))
+
+  function updateStatus(){
+    let totalFollows = 0;
+    for (const v of followsMap.values()) {
+      totalFollows += v.length
+    }
+    const averageFollowsCount = totalFollows / followsMap.size
+
+    const unWeightedFollowCount = new DefaultMap<string, number>(() => 0)
+    const weightedFollowCount = new DefaultMap<string, number>(() => 0)
+    for (const [, follows] of followsMap.entries()) {
+      const multiplier = averageFollowsCount / follows.length
+      for (const follow of follows) {
+        unWeightedFollowCount.set(follow, unWeightedFollowCount.get(follow) + 1)
+        weightedFollowCount.set(follow, weightedFollowCount.get(follow) + multiplier)
+      }
+    }
+
+    setUnweighted([...unWeightedFollowCount.entries()]
+      .sort(profileSortDescending)
+      .map(e => ({actor: e[0], score: e[1]})))
+    setWeighted([...weightedFollowCount.entries()]
+      .sort(profileSortDescending)
+      .map(e => ({actor: e[0], score: e[1]})))
+    const formatter = new Intl.NumberFormat(undefined, {maximumFractionDigits: 2})
+    // devlog([...followsMap.entries()])
+    setStatistics([
+      ["Users Processed", `${(myFollowsList!.length - workQueue.length)}/${myFollowsList!.length}`],
+      ["Total Follows", `${totalFollows}`],
+      ["Average Follows per User", `${formatter.format(averageFollowsCount)}`],
+      ["Unique Users", `${globalUserLookup.size}`],
+      ["Seconds Elapsed", `${(performance.now() - startTime)/1000}`]
+    ])
+    lastUpdate = performance.now()
+  }
 
   while (workQueue.length > 0 && lock == startTime) {
     setRunning(true)
@@ -55,55 +105,24 @@ export async function followsFollows(
       throw new Error("Failed to fetch your follows follows")
     }
     const {cursor, follows} = data
-    if (cursor) {
-      workQueue.push({actor, work: getFollows(actor, cursor)})
-    }
 
     if (follows.length > 0) {
       followsMap.get(actor).push(...follows)
     }
 
-    if (performance.now() - lastUpdate > 100 || workQueue.length == 0) {
-      let totalFollows = 0;
-      for (const v of followsMap.values()) {
-        totalFollows += v.length
+    if (cursor) {
+      workQueue.push({actor, work: getFollows(actor, cursor)})
+    } else {
+      if (!globalFollowsMap.has(actor)) {
+        globalFollowsMap.set(actor, follows)
       }
-      const averageFollowsCount = totalFollows / followsMap.size
+    }
 
-      const unWeightedFollowCount = new DefaultMap<string, number>(() => 0)
-      const weightedFollowCount = new DefaultMap<string, number>(() => 0)
-      for (const [, follows] of followsMap.entries()) {
-        const multiplier = averageFollowsCount / follows.length
-        for (const follow of follows) {
-          unWeightedFollowCount.set(follow, unWeightedFollowCount.get(follow) + 1)
-          weightedFollowCount.set(follow, weightedFollowCount.get(follow) + multiplier)
-        }
-      }
-
-      setUnweighted([...unWeightedFollowCount.entries()]
-        .sort(profileSortDescending)
-        .map(e => ({
-          actor: e[0],
-          score: e[1]
-        })))
-      setWeighted([...weightedFollowCount.entries()]
-        .sort(profileSortDescending)
-        .map(e => ({
-          actor: e[0],
-          score: e[1]
-        })))
-      const formatter = new Intl.NumberFormat(undefined, {maximumFractionDigits: 2})
-      // devlog([...followsMap.entries()])
-      setStatistics([
-        ["Users Processed", `${(myFollowsResponse.data.follows.length - workQueue.length)}/${myFollowsResponse.data.follows.length}`],
-        ["Total Follows", `${totalFollows}`],
-        ["Average Follows per User", `${formatter.format(averageFollowsCount)}`],
-        ["Unique Users", `${globalUserLookup.size}`],
-        ["Seconds Elapsed", `${(performance.now() - startTime)/1000}`]
-      ])
-      lastUpdate = performance.now()
+    if (performance.now() - lastUpdate > 100){
+      updateStatus()
     }
   }
+  updateStatus()
   setRunning(false)
 }
 
